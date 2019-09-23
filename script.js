6eefd84b2d2ecc2260a4a853bf3787135b7c7fdc
Miles.Hamilton <46730797+MilesHamilton@users.noreply.github.com>
2019-09-23 15:07:33 -0400

    set up skeleton for game

---
+++ script.js
@@ -0,0 +1,36 @@
+winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8],
+    [1,4,7],
+]
+
+
+
+
+// creating array from html document using from
+
+
+// turning the q strings into numbers and using replace method to make them empty within their id
+
+
+// returning and array WITHOUT the x's or o's in it using filter function
+
+
+// using .every to detect winning comboos without empty strings
+
+
+//function that takes a _q(quadrant) and a letter (x or o) and makes it equal to the letter via innertext
+
+
+// function that makes the computer randomly choose a number within the empty q's
+
+
+// a function that checks for victory
+
+
+// a functon that displays the opponents choice
